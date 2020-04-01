0203c4125ee7c583c51ff90b3a34ab89124ed2f8
alexNasc <alexmento98@gmail.com>
2020-04-01 19:51:33 +0100

    added get for all countries

--- app.py
+++ app.py
@@ -1,22 +1,82 @@
 #!flask/bin/python
-from flask import Flask,jsonify
+from flask import Flask, jsonify, make_response, g
 from markupsafe import escape
-import connect_database as cd
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = psycopg2.connect(
+            user='postgres', password='CNgrupo8',
+            host='localhost', port='5431')
+    return db
 
-@app.route('/country', methods=['GET'])
+@app.before_request
+def before_request():
+    g.db = get_db()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/countries', methods=['GET'])
 def get_all_countries():
-    return "countries"
+    conn, cur = (g.db, g.db.cursor())
+
+    get_all_countries_query = "SELECT * FROM country;"
+    cur.execute(get_all_countries_query)
+    query_result = cur.fetchall()
+
+    del query_result[0] #remove column names
+
+    countries = []
+    
+    for country in query_result:
+        countries.append(create_country(list(country))) #create country json objet
+
+    r_countries = countries
+
+    r = make_response(jsonify(r_countries))
+    r.status_code = 200
+    
+    return r
+
 
 @app.route('/country/<countryCode>', methods=['GET'])
 def get_country(countryCode):
-    response = cd.get_country_from_db(countryCode)
-    return jsonify(response)
+    conn, cur = (g.db, g.db.cursor())
+
+    get_country_query = "SELECT * FROM country WHERE countrycode=(%s);"
+    data = (countryCode, )
+    cur.execute(get_country_query, data)
+    
+    query_result = cur.fetchone()
+
+    r_country = create_country(list(query_result)) #create country json objet
+
+    r = make_response(jsonify(r_country))
+    r.status_code = 200
+    
+    return r
+
+def create_country(country):
+    country_json = {}
+    country_json['shortname'] = country[1]
+    country_json['longname'] = country[2]
+    country_json['currencyunit'] = country[3]
+    country_json['region'] = country[4]
+    country_json['incomegroup'] = country[5]
+    country_json['latestpopulationcensus'] = country[6]
+    country_json['latesthouseholdsurvey'] = country[7]
+    country_json['latestagriculturalcensusentry'] = country[8]
+    country_json['latestindustrialdata'] = country[9]
+    country_json['latesttradedata'] = country[10]
+
+    return country_json
 
 if __name__ == '__main__':
     app.run(debug=True)
