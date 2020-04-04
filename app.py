5dd74fba8d38970ef35b4546d7b3e9ca2fd8f53f
alexNasc <alexmento98@gmail.com>
2020-04-04 17:39:43 +0100

    changed port

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_db():
     if db is None:
         db = g._database = psycopg2.connect(
             user='postgres', password='CNgrupo8',
-            host='localhost', port='5431')
+            host='localhost', port='5432')
     return db
 
 @app.before_request
