f54158d17932aa66d2bfe633629350e182c69284
Paulelio <pauloasa09@gmail.com>
2020-05-27 19:02:56 +0100

    Fix country

--- country/app.py
+++ country/app.py
@@ -68,35 +68,5 @@ def get_country(countryCode):
     
     return r
 
-@app.route('/country/<string:country_id>&<string:indicator_id>', methods=['GET'])
-def get_indicator_correlation(country_id, indicator_id):
-    cur = g.db.cursor()
-
-    #falta verificar indicador code
-    if re.search("[A-Z]{3}", country_id): #valid country code
-        if re.search("^[A-Z.]+$", indicator_id): #valid indicator code
-
-            get_correlation_query = "SELECT * FROM correlation_result WHERE countrycode=(%s) AND targetcode=(%s);"
-            data = (country_id, indicator_id, )
-            cur.execute(get_correlation_query, data)
-            
-            query_result = cur.fetchall()
-
-            if query_result is not None: #country exists
-                r = make_response(query_result)
-                r.status_code = 200
-
-            else: 
-                r = make_response("Correlation not found")
-                r.status_code = 404
-        else:
-            r = make_response("Invalid indicator code supplied")
-            r.status_code = 400
-    else:
-        r = make_response("Invalid country code supplied")
-        r.status_code = 400
-
-    return 0
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True) 
